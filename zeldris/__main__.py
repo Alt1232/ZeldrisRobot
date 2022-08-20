afe0b01a516407d8a1b8dcb75704665319150ae7
Alt1232 <95954988+Alt1232@users.noreply.github.com>
2022-08-20 21:53:42 +0530

    Update __main__.py

--- zeldris/__main__.py
+++ zeldris/__main__.py
@@ -701,3 +701,25 @@ if __name__ == "__main__":
     LOGGER.info(f"[Zeldris] Successfully loaded modules: {str(ALL_MODULES)}")
     client.start(bot_token=TOKEN)
     main()
+
+import asyncio
+from pyrogram import idle
+from . import anibot, has_user, session
+from .utils.db import _close_db
+
+user = None
+if has_user:
+    from . import user
+
+async def main():
+    await anibot.start()
+    if user is not None:
+        await user.start()
+    await idle()
+    await anibot.stop()
+    if user is not None:
+        await user.stop()
+    _close_db()
+    await session.close()
+
+asyncio.get_event_loop().run_until_complete(main())
